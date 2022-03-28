9783166a4c29dcbd476e49ecd1432be28507f0ab
Shuyang Sun <shuyangsun10@gmail.com>
2022-03-27 21:19:36 -0400

    Solve problem.

--- epi_judge_cpp/is_tree_a_bst.cc
+++ epi_judge_cpp/is_tree_a_bst.cc
@@ -1,12 +1,35 @@
 #include <memory>
+#include <utility>
 
 #include "binary_tree_node.h"
 #include "test_framework/generic_test.h"
 using std::unique_ptr;
 
+std::tuple<bool, int, int> MinAndMax(
+    const unique_ptr<BinaryTreeNode<int>>& tree) {
+  auto res{std::make_tuple(true, tree->data, tree->data)};
+  if (tree->left) {
+    const auto cur{MinAndMax(tree->left)};
+    if (!std::get<0>(cur) || std::get<2>(cur) > tree->data) {
+      return std::make_tuple(false, 0, 0);
+    }
+    std::get<1>(res) = std::get<1>(cur);
+  }
+  if (tree->right) {
+    const auto cur{MinAndMax(tree->right)};
+    if (!std::get<0>(cur) || std::get<1>(cur) < tree->data) {
+      return std::make_tuple(false, 0, 0);
+    }
+    std::get<2>(res) = std::get<2>(cur);
+  }
+  return res;
+}
+
 bool IsBinaryTreeBST(const unique_ptr<BinaryTreeNode<int>>& tree) {
-  // TODO - you fill in here.
-  return true;
+  if (!tree) {
+    return true;
+  }
+  return std::get<0>(MinAndMax(tree));
 }
 
 int main(int argc, char* argv[]) {
